01fc1b661e9fc79d99d1f3cdc4d3ba5db3800793
Marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2024-09-29 16:09:28 +0100

    Add date

---
+++ tests/checkForm.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Select form', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+
+});
+
+test('Fill the content', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+});
+
+
