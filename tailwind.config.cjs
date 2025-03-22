de2ee89282b0da201938ae550b6c54341860b863
Marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2025-03-22 19:26:27 +0000

    Add more style i'm bat with style i think

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				background_color:"#311D3F",
+				content_color:"#522546",
+				content_color_ligth:"#88304E",
+				letter_color:"#E23E57"
+			}
+		},
 	},
 	plugins: [],
 }
