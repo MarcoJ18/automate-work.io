6b2da1f42bbd2ca54140e18bcf71fc53eb14badc
Marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2025-04-06 19:10:13 +0100

    Fix mobile and selector

--- src/scripts/copy.js
+++ src/scripts/copy.js
@@ -3,23 +3,28 @@ class CopyDataHandler{
         this.inputIds = inputId;
         this.copyButton = document.getElementById("copy");
         this.init();
+
     }
 
     init() {
+        this.copyButton.addEventListener("click", () => this.selectorData());
         this.copyButton.addEventListener("click", () => this.handleCopyClick());
+        
       }
     
-    
+
+
       //Copy element
     
-      handleCopyClick(data){
+      handleCopyClick(value){
 
-        const selectValue = document.getElementById("options")
 
+        console.log("Valor seleccionado:", selectedValue);
+        
         setTimeout(async()=>{
-            await navigator.clipboard.writeText(data)
+            await navigator.clipboard.writeText(value)
             .then(() => {
-                alert(selectValue.value);
+                alert(value);
             })
             .catch((error) => {
                 alert('Failed to copy content to clipboard: ' + error);
@@ -27,6 +32,27 @@ class CopyDataHandler{
         },100)    
         
       }
+
+
+      selectorData(){
+
+        const selectElement = document.getElementById("options").value;
+        const selectElementMobile = document.querySelector("#mobileSelector #options").value;
+
+
+
+        if(selectElementMobile == 'Términos de uso' || selectElement == 'Términos de uso'){
+           console.log('si');
+        
+
+        }
+        else if(selectElementMobile == 'Aviso legal' || selectElement == 'Aviso legal'){
+            console.log('que');
+
+        }
+      }
+
+
 }
 
 
