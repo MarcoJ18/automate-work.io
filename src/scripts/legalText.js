6fba7667473b1cfd90c49eb467490c25e3f47d02
Marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2025-04-29 21:35:37 +0100

    New Format

--- src/scripts/legalText.js
+++ src/scripts/legalText.js
@@ -62,3 +62,52 @@ export const legalTexts = {
 `;
     }
 };
+
+
+// Example
+
+/*
+
+
+<h2 style="text-align: center;font-weight: bolder;">Aviso Legal</h2>
+
+<strong></strong><strong>1. Información corporativa. </strong>En virtud de las obligaciones establecidas por la Ley 34/2002, de Servicios de la Sociedad de la Información y de Comercio Electrónico, le informamos que el presente sitio web es propiedad de Nadia Martín García con C.I.F. 78599790G con domicilio en C/ Dulcinea, 136 B,Puerto del Rosario,Las Palmas y con los siguientes datos de contacto: 622178738/ nadiamnga@gmail.com.
+
+<strong>2. Protección de contenidos. </strong>El usuario reconoce y acepta que todos los derechos de propiedad industrial e intelectual sobre los contenidos y/o cualesquiera otros elementos insertados por Nadia Martín García en el presente Sitio Web (incluyendo, a título meramente enunciativo y no limitativo, todos aquellos elementos que conforman la apariencia visual, imagen gráfica y otros estímulos sensoriales del sitio web o «look and feel»: marcas, logotipos, nombres comerciales, textos, imágenes, gráficos, diseños, sonidos, bases de datos, software, diagramas de flujo, presentación, arquitectura de navegación, así como los códigos fuente de las páginas web) pertenecen a Nadia Martín García y/o a terceros a los cuales les han cedido sus derechos.
+
+En ningún caso el acceso al Sitio Web implica algún tipo de permiso, renuncia, transmisión, licencia o cesión total ni parcial de dichos derechos por parte de sus titulares, salvo que se establezca expresamente lo contrario. Los presentes términos y condiciones de uso del Sitio Web no confieren a los USUARIOS ningún otro derecho de utilización, alteración, explotación, reproducción, distribución o comunicación pública del Sitio Web y/o de sus contenidos distintos de los aquí expresamente previstos.
+
+Queda terminantemente prohibida la utilización de tales elementos, su total o parcial reproducción, comunicación y/o distribución con fines comerciales o lucrativos, así como su modificación, alteración, descompilación y/o cualquier otro acto de explotación del Sitio Web.
+
+Sin perjuicio de todo lo anterior, si el Usuario o un tercero estima que algún contenido del Sitio Web pudiera vulnerar derechos de propiedad intelectual e industrial, rogamos así lo pongan en nuestro conocimiento a la mayor brevedad posible.
+
+<strong>3. Acceso y uso del sitio web. </strong>Tanto el acceso al Sitio Web como el uso no consentido que pueda efectuarse de la información contenida en el mismo es de la exclusiva responsabilidad de quien lo realiza.
+
+El usuario se compromete a utilizar los contenidos, información y datos del Sitio Web de conformidad con las presentes condiciones, términos y políticas, con la normativa de aplicación y con las buenas costumbres generalmente aceptadas y el orden público.
+
+El usuario se obliga a abstenerse de utilizar los contenidos del Sitio Web con fines o efectos ilícitos, prohibidos o contrarios a los aquí establecidos, lesivos de los derechos e intereses de Nadia Martín García, de los demás usuarios, de terceros o que de cualquier forma puedan dañar, inutilizar, sobrecargar o deteriorar el presente Sitio Web o impedir la normal utilización o disfrute del mismo por parte de los usuarios.
+
+Nadia Martín García no responderá de ninguna consecuencia, daño o perjuicio que pudiera derivarse de dicho acceso o uso o del incumplimiento de las presentes condiciones, términos y políticas ni se hará responsable de los errores de seguridad que se puedan producir ni de los daños que puedan causarse al sistema informático del usuario (hardware y software) o a los ficheros o documentos almacenados en el mismo como consecuencia de: (i) la presencia de un virus en el ordenador del usuario que sea utilizado para la conexión a los servicios y/o productos ofrecidos por Nadia Martín García a través de su Sitio Web; (ii) un mal funcionamiento del navegador; (iii) el uso de versiones no actualizadas del mismo.
+
+<strong>4. Enlaces a terceros. </strong>En el presente Sitio Web se pueden utilizar enlaces con otras páginas o sitios web. Nadia Martín García no se responsabiliza ni del contenido ni de las medidas de seguridad adoptadas por cualquier otra página o sitio web a través del cual se tiene acceso desde el presente Sitio Web, sitios a los que accede el interesado bajo su exclusiva responsabilidad.
+
+Asimismo, tampoco se garantiza la ausencia de virus u otros elementos en los contenidos enlazados desde el Sitio Web de Nadia Martín García que puedan producir alteraciones en el sistema informático (hardware y software) y/o en los documentos o los ficheros del Usuario, excluyendo asimismo a Nadia Martín García de toda responsabilidad derivada de los daños de cualquier índole ocasionados por todo lo anterior.
+
+<strong>5. Redes sociales.</strong>
+
+Le informamos qué Nadia Martín García puede tener presencia en redes sociales. El tratamiento de los datos que se lleve a cabo de las personas que se hagan seguidoras en las redes sociales (y/o realicen cualquier vínculo o acción de conexión a través de las redes sociales) de las páginas oficiales de Nadia Martín García se regirá por este apartado, así como por aquellas condiciones de uso, políticas de privacidad y normativas de acceso que pertenezcan a la red social que proceda en cada caso y aceptadas previamente por el usuario.
+
+Nadia Martín García tratará sus datos con las finalidades de administrar correctamente su presencia en la red social, informándole de actividades productos o servicios de Nadia Martín García, así como para cualquier otra finalidad que las normativas de las Redes Sociales permitan.
+
+Queda prohibida la publicación de contenidos:
+<ul>
+	<li>Que sean presuntamente ilícitos por la normativa nacional, comunitaria o internacional o que realicen actividades presuntamente ilícitas o contravengan los principios de la buena fe.</li>
+	<li>Que atenten contra los derechos fundamentales de las personas, falten a la cortesía en la red, molesten o puedan generar opiniones negativas en nuestros usuarios o terceros y en general cualesquiera sean los contenidos que Nadia Martín García considere no apropiados.</li>
+	<li>Y en general que contravengan los principios de legalidad, honradez, responsabilidad, protección de la dignidad humana, protección de menores, protección del orden público, la protección de la vida privada, la protección del consumidor y los derechos de propiedad intelectual e industrial.</li>
+</ul>
+Asimismo, Nadia Martín García se reserva la potestad de retirar, sin previo aviso del sitio web o de la red social corporativa aquellos contenidos que se consideren no apropiados.
+
+<strong>6. Modificación de AVISO LEGAL, Política de Privacidad y de Protección de Datos. </strong>El Responsable del Fichero se reserva el derecho a modificar en todo momento y sin previo aviso las presentes condiciones, términos y políticas de privacidad para adaptarlas a las novedades legislativas o jurisprudenciales así como a las modificaciones o prácticas de la industria, debiendo el usuario consultar periódicamente las presentes condiciones, términos y políticas a fin de comprobar o cerciorarse de la existencia de cambios en las mismas, tomando como referencia la fecha de la última actualización.
+
+
+*/
